727b99fa24dadbe1f5322b397e7494a0b2fe94d4
RJ Gonzalez <carsimdeliez@gmail.com>
2022-02-25 21:38:28 +0800

    extend encoder to 617 parallel input and outputs

--- quantizer and lut/Encoder.v
+++ quantizer and lut/Encoder.v
@@ -2,29 +2,39 @@
 `include "IM_Fetch.v"
 
 module Encoder(input_value, hv, clk, en, nrst);
-  input [31:0]input_value;
+  input [616:0][31:0]input_value;
   input clk, en, nrst;
-  output reg [9:0]hv;
-  wire [3:0]quantized_value_level;
-  wire [9:0]im_fetch_output;
+  output reg [616:0][9:0]hv;
+  wire [616:0][3:0]quantized_value_level;
+  wire [616:0][9:0]im_fetch_output;
   
   // Declare and initialize Item Memory
   reg [9:0][9:0] im_hvs;    
   
-  Quantizer qtz(
-    .input_value(input_value),
-    .nrst(nrst),
-    .en(en),
-    .quantized_value_level(quantized_value_level)
-  );
+  generate
+    genvar i;
+    for (i=0; i<617; i=i+1) begin : qtzs
+      Quantizer qtz(
+        .input_value(input_value[i][31:0]),
+        .nrst(nrst),
+        .en(en),
+        .quantized_value_level(quantized_value_level[i][3:0])
+      );
+    end
+  endgenerate
   
-  IM_Fetch im(
-    .im_hvs(im_hvs),
-    .qlevel(quantized_value_level),
-    .en(en),
-    .nrst(nrst),
-    .level_hv(im_fetch_output)
-  );
+  generate
+    genvar j;
+    for (j=0; j<617; j=j+1) begin : ims
+      IM_Fetch im(
+        .im_hvs(im_hvs),
+        .qlevel(quantized_value_level[j][3:0]),
+        .en(en),
+        .nrst(nrst),
+        .level_hv(im_fetch_output[j][9:0])
+      );
+    end
+  endgenerate
   
   initial begin
     im_hvs[0][9:0] = 10'b0000000000;
@@ -41,9 +51,10 @@ module Encoder(input_value, hv, clk, en, nrst);
   
   always@(posedge clk) begin
     if(!nrst)
-      hv = 10'b0000000000;
+      hv <= 0;
     else
-      if(en)
-        hv = im_fetch_output;
+      if(en) begin
+        hv <= im_fetch_output;
+      end
   end
 endmodule
--- quantizer and lut/TB_Encoder.sv
+++ quantizer and lut/TB_Encoder.sv
@@ -1,7 +1,7 @@
 module tb_encoder();
-  reg [31:0]input_value;
+  reg [616:0][31:0]input_value;
   reg clk, en, nrst;
-  wire [9:0]hv;
+  wire [616:0][9:0]hv;
   
   Encoder UUT(
     .input_value(input_value),
@@ -12,104 +12,116 @@ module tb_encoder();
   );
   
 initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(1);
+  $dumpfile("dump.vcd");
+  $dumpvars(1);
     
-    clk = 0;
-    en = 0;
+  clk = 0;
+  en = 0;
     
-    #1
-    nrst = 1;
-    en = 1;
+  #1
+  nrst = 1;
+  en = 1;
 // Scenario : > 0.8889
-    input_value = 32'b00111111100000000000000000000000; //1.0
-    #1
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b00111111100000000000000000000000; //1.0
+  #1
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : 0.8889 > x > 0.66667
-    input_value = 32'b00111111001100110011001100110011; //0.7
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b00111111001100110011001100110011; //0.7
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : 0.66667 > x > 0.44444
-    input_value = 32'b00111111000000000000000000000000; //0.5
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b00111111000000000000000000000000; //0.5
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : 0.44444 > x > 0.22222
-    input_value = 32'b00111110100110011001100110011010; //0.3
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b00111110100110011001100110011010; //0.3
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
   
 // Scenario : 0.22222 > x > 0
-    input_value = 32'b00111101110011001100110011001101; //0.1
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b00111101110011001100110011001101; //0.1
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : 0 > x > -0.22222
-    input_value = 32'b10111101110011001100110011001101; //-0.1
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b10111101110011001100110011001101; //-0.1
+  #2
+  clk = 1;
+  $display("%b", hv[0][9:0]);
+  #2
+  clk = 0;
     
 // Scenario : -0.22222 > x > -0.44444
-    input_value = 32'b10111110100110011001100110011010; //-0.3
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b10111110100110011001100110011010; //-0.3
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : -0.44444 > x > -0.66667
-    input_value = 32'b10111111000000000000000000000000; //-0.5
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b10111111000000000000000000000000; //-0.5
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : -0.66667 > x > -0.88889
-    input_value = 32'b10111111001100110011001100110011; //-0.7
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b10111111001100110011001100110011; //-0.7
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
     
 // Scenario : x < -0.88889
-    input_value = 32'b10111111100000000000000000000000; //-1.0
-    #2
-    clk = 1;
-    #2
-    clk = 0;
+  input_value[0][31:0] = 32'b10111111100000000000000000000000; //-1.0
+  #2
+  clk = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk = 0;
 
 // // Scenario : nrst goes down
-    input_value = 32'b10111111001100110011001100110011; //-0.7
-    nrst = 0;
-    #2
-    clk=1;
-    nrst = 1;
-    #2
-    clk=0;
+  input_value[0][31:0] = 32'b10111111001100110011001100110011; //-0.7
+  nrst = 0;
+  #2
+  clk=1;
+  nrst = 1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk=0;
     
 // Scenario : enable is 0 when nrst is 1
-	input_value = 32'b10111111100000000000000000000000; //-1.0
-    en = 0;
-    #2
-    clk=1;
-    #2
-    clk=0;
-    #2
-    clk = 1;
+  input_value = 32'b10111111100000000000000000000000; //-1.0
+  en = 0;
+  #2
+  clk=1;
+  #2
+  $display("%b", hv[0][9:0]);
+  clk=0;
+  #2
+  clk = 1;
 
-    $finish;
+  $finish;
   end
 endmodule
