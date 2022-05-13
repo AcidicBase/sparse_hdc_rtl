a2761f01ba15528ab0b36bcc125c9515801bcb10
AcidicBase <fc122097@gmail.com>
2022-05-13 12:51:40 +0800

    assoc mem & class gen w/o latches
    The files you need to replace in the class hv gen and assoc mem so that they have no latches

---
+++ class hv gen & assoc mem no latches/assoc_mem_top.sv
@@ -0,0 +1,108 @@
+`include "assoc_mem_fsm.sv"
+`include "am_and_array.sv"
+`include "am_tree_adder.sv"
+`include "am_tree_comparator.sv"
+`include "am_accuracy_tally.sv"
+
+module assoc_mem_top(
+    input wire clk,
+    input wire nrst,
+    input wire en,
+    input wire start_querying,
+    input wire testing_hdc_model,
+    input wire testing_dataset_finished,
+    input wire [HV_DIM-1:0] query_hv,
+    input wire [SEQ_CYCLE_COUNT-1:0][DIMS_PER_CC-1:0] binary_class_hvs [0:25],
+    input wire [4:0] correct_class,
+    output wire [4:0] class_inference,
+    output wire [10:0] number_of_correct_inferences
+    );
+    
+    // FSM signals
+    wire [3:0] query_ctr;
+    wire comparing_query_hv_with_class_hv;
+    wire inferring_class;
+    wire tallying_accuracy;
+    
+    // AND array output
+    logic [DIMS_PER_CC-1:0] query_hv_segment;
+    wire [DIMS_PER_CC-1:0] and_array_out [0:25];
+    
+    // Tree adder outputs
+    wire [12:0] similarity_values [0:25];           // 13 bits to represent 5k-dim hvs
+    
+    // FSM
+    assoc_mem_fsm AM_FSM_1(
+        .clk(clk),
+        .nrst(nrst),
+        .en(en),
+        .start_querying(start_querying),
+        .testing_dataset_finished(testing_dataset_finished),
+        .query_ctr(query_ctr),
+        .comparing_query_hv_with_class_hv(comparing_query_hv_with_class_hv),
+        .inferring_class(inferring_class),
+        .tallying_accuracy(tallying_accuracy)
+    );
+    
+    // AND array
+    am_and_array AM_AND1(
+        .comparing_query_hv_with_class_hv(comparing_query_hv_with_class_hv),
+        .query_ctr(query_ctr),
+        .query_hv_segment(query_hv_segment),
+        .binary_class_hvs(binary_class_hvs),
+        .and_array_out(and_array_out)
+    );
+    
+    // Tree Adders
+    for (genvar i = 0; i < 26; i++) begin
+        am_tree_adder AM_TREE_ADD1(
+            .clk(clk),
+            .nrst(nrst),
+            .comparing_query_hv_with_class_hv(comparing_query_hv_with_class_hv),
+            .inferring_class(inferring_class),
+            .and_array_out(and_array_out[i]),
+            .similarity_value(similarity_values[i])
+        );
+    end
+
+    // Tree Comparator
+    am_tree_comparator AM_COMP1(
+        .clk(clk),
+        .nrst(nrst),
+        .inferring_class(inferring_class),
+        .similarity_values(similarity_values),
+        .class_inference(class_inference)
+    );
+    
+    // Tally
+    am_accuracy_tally TALLY_1(
+        .clk(clk),
+        .nrst(nrst),
+        .tallying_accuracy(tallying_accuracy),
+        .correct_class(correct_class),
+        .class_inference(class_inference),
+        .number_of_correct_inferences(number_of_correct_inferences)
+    );
+    
+    // input mux
+    always_comb begin
+		if (testing_hdc_model) begin
+		    case(query_ctr)
+				4'd0:    query_hv_segment = query_hv[499:0];
+		        4'd1:    query_hv_segment = query_hv[999:500];
+		        4'd2:    query_hv_segment = query_hv[1499:1000];
+		        4'd3:    query_hv_segment = query_hv[1999:1500];
+		        4'd4:    query_hv_segment = query_hv[2499:2000];
+		        4'd5:    query_hv_segment = query_hv[2999:2500];
+		        4'd6:    query_hv_segment = query_hv[3499:3000];
+		        4'd7:    query_hv_segment = query_hv[3999:3500];
+		        4'd8:    query_hv_segment = query_hv[4499:4000];
+		        default: query_hv_segment = query_hv[4999:4500];
+		    endcase
+		end 
+		else begin
+			query_hv_segment = 500'd0;
+		end
+    end  
+
+endmodule
---
+++ class hv gen & assoc mem no latches/class_hv_gen_top.sv
@@ -0,0 +1,126 @@
+`include "class_bundler.sv"
+`include "class_thresholder.sv"
+`include "class_fsm.sv"
+`include "nonbinary_class_reg.sv"
+`include "binary_class_reg.sv"
+
+module class_hv_gen_top(
+    input wire clk,
+    input wire nrst,
+    input wire en,
+    input wire start_class_gen,
+	input wire training_hdc_model,
+    input wire training_dataset_finished,  
+    input wire [HV_DIM-1:0] encoded_hv,
+    input wire [4:0] class_select_bits,
+    output wire [SEQ_CYCLE_COUNT-1:0][DIMS_PER_CC-1:0] bin_class_hvs [0:25],
+    output wire class_gen_done
+    );
+    
+    // FSM internal signals
+    wire [3:0] nonbin_ctr;
+    wire [3:0] bin_ctr;
+    wire [3:0] class_hv_gen_ctr;
+    wire [4:0] binarized_class_counter;
+    wire adjusting_nonbin_class_hvs;
+    wire binarizing_class_hvs;
+    
+    // Bundler internal signals
+    logic [DIMS_PER_CC-1:0] input_hv_chunk;
+	wire  [DIMS_PER_CC-1:0][BITWIDTH_PER_DIM-1:0] stored_hv_chunk;
+    
+    // nonbinary_class_reg internal signals
+    wire [DIMS_PER_CC-1:0][BITWIDTH_PER_DIM-1:0] nonbin_class_reg_out;
+    wire [DIMS_PER_CC-1:0][BITWIDTH_PER_DIM-1:0] nonbin_class_reg_in;
+	wire  [4:0] nonbin_class_select_bits;
+    
+    // binary_class_reg internal signals
+    wire [DIMS_PER_CC-1:0] bin_class_reg_in;
+
+    
+    // instantiate fsm
+    class_fsm C_FSM_1(
+        .clk(clk),
+        .nrst(nrst),
+        .en(en),
+        .start_class_gen(start_class_gen),
+        .training_dataset_finished(training_dataset_finished),
+        .nonbin_ctr(nonbin_ctr),
+        .bin_ctr(bin_ctr),
+        .class_hv_gen_ctr(class_hv_gen_ctr),
+        .binarized_class_counter(binarized_class_counter),
+        .adjusting_nonbin_class_hvs(adjusting_nonbin_class_hvs),
+        .binarizing_class_hvs(binarizing_class_hvs),
+        .class_gen_done(class_gen_done)
+    );
+    
+    // instantiate class_bundler
+    class_bundler C_BUNDLE_1(
+        .input_hv_chunk(input_hv_chunk),
+        .stored_hv_chunk(stored_hv_chunk),
+        .sum(nonbin_class_reg_in)
+    );
+
+	assign stored_hv_chunk = (training_hdc_model) ? nonbin_class_reg_out : 4500'b0;	
+    
+    // instantiate class_thresholder
+    class_thresholder C_THR_1(
+        .nrst(nrst),
+        .en(en),
+        .binarizing_class_hvs(binarizing_class_hvs),
+        .nonbin_class_reg_out(nonbin_class_reg_out),
+        .thresholded_hv(bin_class_reg_in)
+    );
+    
+    // instantiate nonbinary_class_reg
+    nonbinary_class_reg NONBIN_CLASS_1(
+        .clk(clk),
+        .nrst(nrst),
+        .en(en),
+        .adjusting_nonbin_class_hvs(adjusting_nonbin_class_hvs),
+        .class_select_bits(class_select_bits),
+		.nonbin_class_select_bits(nonbin_class_select_bits),
+        .nonbin_ctr(nonbin_ctr),
+        .class_hv_gen_ctr(class_hv_gen_ctr),
+        .nonbin_class_reg_in(nonbin_class_reg_in),
+        .nonbin_class_reg_out(nonbin_class_reg_out)
+    );
+    
+    // instantiate binary_class_reg
+    binary_class_reg BIN_CLASS_1(
+        .clk(clk),
+        .nrst(nrst),
+        .en(en),
+        .binarizing_class_hvs(binarizing_class_hvs),
+        .binarized_class_counter(binarized_class_counter),
+        .bin_ctr(bin_ctr),
+        .bin_class_reg_in(bin_class_reg_in),
+        .bin_class_hvs(bin_class_hvs)
+    );
+    
+    // input mux
+    always_comb begin
+		if(training_hdc_model) begin
+		    case(nonbin_ctr)
+				4'd0:    input_hv_chunk = encoded_hv[499:0];
+		        4'd1:    input_hv_chunk = encoded_hv[999:500];
+		        4'd2:    input_hv_chunk = encoded_hv[1499:1000];
+		        4'd3:    input_hv_chunk = encoded_hv[1999:1500];
+		        4'd4:    input_hv_chunk = encoded_hv[2499:2000];
+		        4'd5:    input_hv_chunk = encoded_hv[2999:2500];
+		        4'd6:    input_hv_chunk = encoded_hv[3499:3000];
+		        4'd7:    input_hv_chunk = encoded_hv[3999:3500];
+		        4'd8:    input_hv_chunk = encoded_hv[4499:4000];
+		        default: input_hv_chunk = encoded_hv[4999:4500];
+		    endcase
+		end 
+		else begin
+			input_hv_chunk = 500'd0;
+		end
+    end 
+
+	// select bits of nonbinary class mux
+	assign nonbin_class_select_bits = (binarizing_class_hvs == 1'b1) ? binarized_class_counter : class_select_bits;
+
+    
+endmodule
