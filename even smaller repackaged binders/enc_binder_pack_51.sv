d175411f3060c67bb1ab4fc3adbdd71b0d5439fc
AcidicBase <fc122097@gmail.com>
2022-05-25 19:41:25 +0800

    Binders in groups of 10
    Just in case groups of 31 still causes that weird super threading error. Just replace encoding_top and the binder packs accordingly.

---
+++ even smaller repackaged binders/enc_binder_pack_51.sv
@@ -0,0 +1,21 @@
+module enc_binder_pack_51(
+    input  wire  					clk,
+    input  wire  					nrst,
+    input  wire  					start_encoding,
+    input  wire  	[HV_DIM-1:0] 	level_hv		[0:9],
+    output logic	[HV_DIM-1:0] 	shifted_hv		[0:9]
+);
+                      
+    for (genvar i = 0; i < 10; i++) 
+		begin : binders
+			enc_binder #(.SHIFT(SHIFTS[510 + i]))
+			ENC_BINDER(
+			    .clk							(clk),
+			    .nrst							(nrst),
+			    .start_binding					(start_encoding),
+			    .level_hv						(level_hv[i]),
+			    .shifted_hv						(shifted_hv[i])
+			);
+    	end	
+	
+endmodule
