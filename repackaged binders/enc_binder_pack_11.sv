fed69dbb513ea352af2a5a09c754ba2995a76bca
AcidicBase <fc122097@gmail.com>
2022-05-25 18:01:30 +0800

    Made each binder pack smaller
    Genus keeps losing connection to super-threading server during synthesis. Just adjust encoding_top accordingly.

---
+++ repackaged binders/enc_binder_pack_11.sv
@@ -0,0 +1,21 @@
+module enc_binder_pack_11(
+    input  wire  					clk,
+    input  wire  					nrst,
+    input  wire  					start_encoding,
+    input  wire  	[HV_DIM-1:0] 	level_hv		[0:((FEATURES_PER_CC/2)-1)],
+    output logic	[HV_DIM-1:0] 	shifted_hv		[0:((FEATURES_PER_CC/2)-1)]
+);
+                      
+    for (genvar i = 0; i < (FEATURES_PER_CC/2); i++) 
+		begin : binders
+			enc_binder #(.SHIFT(SHIFTS[341+i]))
+			ENC_BINDER(
+			    .clk							(clk),
+			    .nrst							(nrst),
+			    .start_binding					(start_encoding),
+			    .level_hv						(level_hv[i]),
+			    .shifted_hv						(shifted_hv[i])
+			);
+    	end	
+	
+endmodule
